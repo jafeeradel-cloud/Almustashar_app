6c7619d6133b1c909b8170774230ccf987e28498
jafeeradel-cloud <jafeeradel@gmail.com>
2025-12-12 23:54:05 +0300

    Update Property.cs

--- Property.cs
+++ Property.cs
@@ -1,10 +1,11 @@
-
-namespace RealEstateApi.Models
+namespace Almustashar_app
 {
     public class Property
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public decimal Price { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
     }
 }
