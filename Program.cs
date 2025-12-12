96ec66642c5a68b26f826f34166bb6ab4b0cb490
jafeeradel-cloud <jafeeradel@gmail.com>
2025-12-13 00:57:18 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,11 +15,10 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
